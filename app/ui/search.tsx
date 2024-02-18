d9499af893e040c2a3edbaa374d386c429cac78c
promethean2020 <75622659+promethean2020@users.noreply.github.com>
2024-02-18 19:50:05 +0500

    5th commit

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,14 +1,26 @@
 'use client';
-
+import { useSearchParams } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const handleSearch = (term: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    console.log(params.values, 'Asdasd');
+  };
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
